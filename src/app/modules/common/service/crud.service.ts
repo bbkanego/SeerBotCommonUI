9f6417e6315043e78d42a93dd7de628104143075
bkane <kanebhushan@gmail.com>
2018-12-10 20:56:05 -0500

    More changes for bot launch [bkane]

--- src/app/modules/common/service/crud.service.ts
+++ src/app/modules/common/service/crud.service.ts
@@ -1,49 +1,55 @@
-import { Injector } from '@angular/core';
-import { Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import { HttpClient } from './httpClient.helper';
+import {Injector} from '@angular/core';
+import {Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {HttpClient} from './httpClient.helper';
 
 export abstract class CrudService<T> {
 
-    private httpClient: HttpClient;
+  private httpClient: HttpClient;
 
-    constructor(private injector: Injector) {
-        this.httpClient = this.injector.get(HttpClient);
-    }
+  constructor(private injector: Injector) {
+    this.httpClient = this.injector.get(HttpClient);
+  }
 
-    public abstract initModel(): Observable<T>;
+  public abstract initModel(): Observable<T>;
 
-    public abstract save(model: T): Observable<T>;
+  public abstract save(model: T): Observable<T>;
 
-    public abstract delete(id: string): Observable<T>;
+  public abstract delete(id: string): Observable<T>;
 
-    public abstract update(model: T): Observable<T>;
+  public abstract update(model: T): Observable<T>;
 
-    public abstract getById(id: string): Observable<T>;
+  public abstract getById(id: string): Observable<T>;
 
-    public abstract getAll(): Observable<T[]>;
+  public abstract getAll(): Observable<T[]>;
 
-    protected getRequest(url: string): Observable<T> {
-        return this.httpClient.get(url)
-            .map((res: Response) => res.json())
-            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    }
+  protected getRequest(url: string): Observable<T> {
+    return this.httpClient.get(url)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
 
-    protected postRequest(url: string, model: T) {
-        return this.httpClient.post(url, JSON.stringify(model))
-        .map((res: Response) => res.json())
-        .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    }
+  protected postRequest(url: string, model: T) {
+    return this.httpClient.post(url, JSON.stringify(model))
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
 
-    protected putRequest(url: string, model: T) {
-        return this.httpClient.put(url, JSON.stringify(model))
-        .map((res: Response) => res.json())
-        .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    }
+  protected putRequest(url: string, model: T) {
+    return this.httpClient.put(url, JSON.stringify(model))
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
 
-    protected deleteRequest(url: string, id: string) {
-        return this.httpClient.delete(url, id)
-        .map((res: Response) => res.json())
-        .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    }
+  protected deleteRequest(url: string, id: string) {
+    return this.httpClient.delete(url, id)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  protected postMultiPartRequest(url: string, model: FormData) {
+    return this.httpClient.postMultipart(url, model)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
 }
